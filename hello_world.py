c188c07d613744df18041088361396ba5488e372
Jelmer <jelmerf@holmesgroup.com>
2023-06-08 12:37:15 +0200

    first push

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,15 @@
 print('hello world')
+
+print('i know :)')
+
+my_fruits: dict[str, int] = {
+    "apples": 25,
+    "bananas": 31
+}
+
+def func1(favo: int):
+    print("my favourite integer is {}".format(favo))
+
+func1(45)
+
+
