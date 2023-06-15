a2503e7a138415017768a162c03878b69c64431c
Maricadewit-BAM <marica.de.wit@bam.com>
2023-06-15 12:35:57 +0200

    second_commit

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,39 @@
 print('hello world')
+
+
+i: str = 0
+c: str = 'h'
+string: str = "I am a string"
+
+my_tuple = ("a", "b", "c") # entries can not be changed
+my_list: list[int] = [1, 2, 3, 4] # can be mutated
+
+my_dict: dict[str, int] = {
+    "key1": 1,
+    "key2": 2
+}
+
+print(i)
+print(c)
+print(string)
+print(my_tuple)
+print(my_list)
+print(my_dict)
+
+
+# accessing an element in a list 
+print(my_list[-1])
+
+# accessing an element in a dictionary
+print(my_dict["key1"])
+
+
+def func1():
+    print('I am function 1')
+
+
+def func2(a: int, b: str):
+    print(a, b)
+
+func1()
+func2(b='value', a=999)
