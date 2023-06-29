a0f19218c248871321beacef483f6814d830cc46
Pascal Martens <pascal.martens@bam.com>
2023-06-29 12:34:04 +0200

    Classes, properties, methods

---
+++ main.py
@@ -0,0 +1,33 @@
+# start file
+from typing import Any
+
+# python import
+from model import Model
+from nodes import Node
+
+model: Model = Model("2DFrame")  # instantiate the class
+
+# adding nodes to model
+model.nodes[1] = Node(1,[0., 0., 0.])
+model.nodes[2] = Node(2,[0., 0., 3.])
+model.nodes[3] = Node(3,[4., 0., 3.])
+model.nodes[4] = Node(4,[4., 0., 0.])
+
+#model.node_count = 6
+
+#testZ2 = model.nodes[2].z
+
+# nodes: dict[int, list[float]] = {
+#     1: n1,
+#     2: n2,
+#     3: n3,
+#     4: n4
+# }
+
+# adding bars to model
+model.bars[1] = [1, 2]  # column1
+model.bars[2] = [4, 3]  # columns2
+model.bars[3] = [2, 3]  # beam 1
+
+
+print('model: ', model)
