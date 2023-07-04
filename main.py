7e2d8fbdcf33d04ffe1a831edf0699f56cf6c5ae
Jack English <jack.english@live.ie>
2023-07-04 16:47:18 +0200

    node class added

--- main.py
+++ main.py
@@ -3,22 +3,16 @@ from typing import Any
 
 # python import
 from model import Model
+from nodes import Node
 
 
 model: Model = Model("2DFrame")  # instantiate the class
 
 # adding nodes to model
-model.nodes[1] = [0., 0., 0.]
-model.nodes[2] = [0., 0., 3.]
-model.nodes[3] = [4., 0., 3.]
-model.nodes[4] = [4., 0., 0.]
-
-# nodes: dict[int, list[float]] = {
-#     1: n1,
-#     2: n2,
-#     3: n3,
-#     4: n4
-# }
+model.nodes[1] = Node(1, [0., 0., 0.])
+model.nodes[2] = Node(2, [0., 0., 3.])
+model.nodes[3] = Node(3, [4., 0., 3.])
+model.nodes[4] = Node(4, [4., 0., 0.])
 
 # adding bars to model
 model.bars[1] = [1, 2]  # column1
--- model.py
+++ model.py
@@ -1,11 +1,72 @@
+from nodes import Node
+
 class Model():
 
     # constructor
     def __init__(self, model_name: str):
         self.name = model_name
 
-        self.nodes: dict[int, list[float]] = dict()
+        self.nodes: dict[int, Node] = dict()
         self.bars: dict[int, list[int]] = dict()
 
     def print_name(self):
         print(self.name)
+
+    @property
+    def p_node_count(self):
+        return len(self.nodes)
+
+    @p_node_count.setter
+    def p_node_count(self, number: int):
+        raise Exception("you should not modify the node count directly")
+
+    @property
+    def p_bar_count(self):
+        return len(self.bars)
+
+
+
+####################################### test  #################################
+
+class DemoClass():
+
+    def __init__(self, name: str = "default"):
+        self.my_name = name
+
+    def print_name(self):
+        print(self.my_name)
+
+    @staticmethod
+    def print_this_name(name: str):
+        print(name)
+
+    # basic getter
+    def get_name(self):
+        print(self.my_name)
+
+    @property # getter
+    def name(self):
+        return self.my_name + "_" + "2"
+
+    # basic setter
+    def set_name(self, new_name: str):
+        self.my_name = new_name
+
+    @name.setter
+    def name(self, new_name: str):
+        self.my_name = new_name
+
+
+
+if __name__ == "__main__":
+
+    demo = DemoClass("jake") # instantiation / creating an instance
+
+    # demo.set_name('Jelmer')
+
+    # print("name property: ", demo.name)
+
+    demo.name = "jake_3"
+
+
+    DemoClass.print_this_name("maria")
