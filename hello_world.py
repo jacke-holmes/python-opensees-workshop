8a5153006f783dfb7a3c7a7f0a20876772976f16
alistairb-holmes <alistairb@holmesgroup.com>
2023-07-05 13:56:20 +0200

    first commit

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,38 @@
 print('hello world')
+
+i = 0
+c = "h"
+string = "i am a string"
+
+my_list = [1,2,3,4]
+
+my_tuple = ("a", "b", "c")
+
+my_dict = {
+    "key1": 1,
+    "key2": 2
+}
+
+my_list2: list[int] = [1,2,3,4]  # position/label 0,1,2,3
+
+
+print(i)
+print(c)
+print(string)
+print(my_list2)
+print(my_tuple)
+print(my_dict)
+
+
+# accessing an element in a list
+print(my_list[0])
+
+# accessing an element in a dictionary
+print(my_dict["key2"])
+
+# functions
+
+def func1():
+    print(my_list2)
+
+func1()
