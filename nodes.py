75c7ff6fe967279829f559c28c28872b99fd8de8
Pascal Martens <pascal.martens@bam.com>
2023-07-06 12:37:18 +0200

    Progress 6-7-23

--- nodes.py
+++ nodes.py
@@ -1,8 +1,10 @@
-class Node():
+from base_classes.thing import Thing
+
+class Node(Thing):
 
     # constructor 
     def __init__(self, node_id: int, node_coords: list[float]):
-        self.node_id = node_id
+        super().__init__(node_id)
 
         assert len(node_coords) == 3, "You must have 3 coordinates"
         self.x: float = node_coords[0]
@@ -11,3 +13,11 @@ class Node():
     
     def __repr__(self) -> str:
         return f"Node ({self.x, self.y, self.z})"
+    
+    def get_node_coords_as_list(self) -> list[float]:
+        # coord_list: list[float] = list()        
+        # coord_list.append(self.x)
+        # coord_list.append(self.y)
+        # coord_list.append(self.z)
+
+        return [self.x, self.y, self.z]
